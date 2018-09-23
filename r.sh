8f39ef4a29894bf03cab330ed217e62fad1154d2
Rustamspl <Rustamspl@gmail.com>
2018-09-23 14:04:44 +0600

    1

--- r.sh
+++ r.sh
@@ -1,2 +1,8 @@
 #test3
-./configure --disable-doc  --disable-everything --enable-encoder=PCM  --enable-decoder=PCM && make
+./configure \
+--disable-doc \
+--disable-everything \
+--enable-encoder=PCM  \
+--enable-decoder=PCM \
+--enable-demuxer=r264 \
+&& make
